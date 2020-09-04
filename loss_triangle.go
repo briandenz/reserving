aebcc52809db5db3cbf55473e5349c3767cf83b8
briandenz <brian.denz@icloud.com>
2020-09-04 12:42:44 -0400

    flushing out Triangle struct

--- loss_triangle.go
+++ loss_triangle.go
@@ -2,8 +2,13 @@ package reserving
 
 //Triangle : Defines the loss triangle structure used for paid and incurred development
 type Triangle struct {
-	Origin      []byte `json:"triangle_origin"`
-	Development []byte `json:"triangle_development"`
-
-	Cumulative bool `json:"triangle_cum_incr"`
+	Index            []byte   `json:"triangle_index"`
+	Columns          []string `json:"triangle_columns"`
+	Origin           []byte   `json:"triangle_origin"`
+	Development      []byte   `json:"triangle_development"`
+	KeyLabels        []byte   `json:"triangle_key_labels"`
+	Valuation        []byte   `json:"triangle_valuation_dates"`
+	OriginGrain      []string `json:"triangle_origin_grain"`
+	DevelopmentGrain []string `json:"triangle_development_grain"`
+	Shape            [4]int   `json:"triangle_shape"`
 }
